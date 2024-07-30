e3c0ba4b9f2bbf8d2f21849b0c47a89093f3682b
Rushi_0508 <rushigandhi14@gmail.com>
2024-07-30 16:15:58 +0530

    refactor: better code formatting

--- src/main/stringCalculator.ts
+++ src/main/stringCalculator.ts
@@ -5,29 +5,37 @@ export class StringCalculator {
     let delimiter = ",";
     let newLineDelimiter = "//";
     let negativeNumbers: number[] = [];
+
     const lines: string[] = numbers.split(newLine);
+
     for (const line of lines) {
       if (line.startsWith(newLineDelimiter)) {
         delimiter = line.substring(newLineDelimiter.length);
         continue;
       }
+
       if (line.endsWith(delimiter)) {
         throw new Error("Invalid input!");
       }
+
       const lineNumbers = line.split(delimiter);
+
       for (const lineNumber of lineNumbers) {
         if (lineNumber.startsWith("-")) {
           negativeNumbers.push(Number(lineNumber));
           continue;
         }
-        if (negativeNumbers.length === 0) result += Number(lineNumber);
+
+        result += Number(lineNumber);
       }
     }
+
     if (negativeNumbers.length > 0) {
       throw new Error(
         `negative numbers not allowed ${negativeNumbers.join(", ")}`
       );
     }
+
     return result;
   }
 }
